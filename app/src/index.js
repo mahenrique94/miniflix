578e865869edd7cd5f22299f5c9d45adbfb3ff62
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-01-25 22:31:44 -0200

    Started a system route and redux

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux";
+import { Router, Route } from "react-router";
+import createHistory from "history/createBrowserHistory";
+import store from "./store";
+
+import App from './App';
+import Painel from "./components/painel/Painel";
+
 import "./assets/css/normalize.css";
 import "./assets/css/fontawesome/css/fontawesome-all.min.css";
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <Router history={ createHistory() }>
+            <div>
+                <Route path="/" component={ App }/>
+                <Route path="/painel" component={ Painel }/>
+            </div>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ app/src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import thunkMiddleware from "redux-thunk";
+
+export default createStore (
+    reducers,
+    applyMiddleware(thunkMiddleware)
+);
