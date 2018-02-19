dc82ea596a715bd45c632eff82abb0a58b9c56f5
Matheus Castiglioni <matheus.castiglioni@la.logicalis.com>
2018-02-19 15:39:14 -0300

    Improving better design on menu app

--- app/src/components/app/Menu.js
+++ app/src/components/app/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { i18n } from "./../../helpers/i18n";
 
 export default class Menu extends Component {
@@ -7,7 +7,10 @@ export default class Menu extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {hide : false};
+        this.state = {
+            activeMenu: false,
+            hide : true
+        };
         this.dropdown = this.dropdown.bind(this);
     }
 
@@ -18,9 +21,22 @@ export default class Menu extends Component {
                 <div className="mf-menu__wrap">
                     <ul className="mf-menu__list">
                         <li className="mf-menu__item has-dropdown">
-                            <button className="mf-menu__link" onClick={ this.dropdown } type="button"><i className="fas fa-bars mf-menu__icon no-margin"></i></button>
-                            <div className="">
-                            </div>
+                            <button className={`mf-menu__link ${ this.state.activeMenu ? "is-active" : "" }`} onClick={ this.dropdown } type="button"><i className="fas fa-bars mf-menu__icon no-margin"></i></button>
+                            <ul className={ `mf-menu__sub___list ${ this.state.hide ? "is-hide" : "is-show" }` }>
+                                <div className="mf-menu__sub___info">
+                                    <h2 className="mf-menu__sub___title">
+                                        <i className="fab fa-buromobelexperte mf-menu__sub___icon"></i>
+                                        <span className="mf-menu__sub___text">{ i18n.message("menu.cattegories") }</span>
+                                    </h2>
+                                </div>
+                                <div className="mf-menu__sub___itens">
+                                    <li className="mf-menu__subItem"><Link className="mf-menu__sub___link" to="/cattegory/action">{ i18n.message("menu.action") }</Link></li>
+                                    <li className="mf-menu__subItem"><Link className="mf-menu__sub___link" to="/cattegory/comedy">{ i18n.message("menu.comedy") }</Link></li>
+                                    <li className="mf-menu__subItem"><Link className="mf-menu__sub___link" to="/cattegory/horror">{ i18n.message("menu.horror") }</Link></li>
+                                    <li className="mf-menu__subItem"><Link className="mf-menu__sub___link" to="/cattegory/romance">{ i18n.message("menu.romance") }</Link></li>
+                                    <li className="mf-menu__subItem"><Link className="mf-menu__sub___link" to="/cattegory/thriller">{ i18n.message("menu.thriller") }</Link></li>
+                                </div>
+                            </ul>
                         </li>
                     </ul>
                     <ul className="mf-menu__list">
@@ -31,8 +47,11 @@ export default class Menu extends Component {
         );
     }
 
-    dropdown() {
-        this.setState({hide : !this.state.hide});
+    dropdown(event) {
+        this.setState({
+            activeMenu : !this.state.activeMenu,
+            hide : !this.state.hide
+        });
     }
 
 }
